38445d96abcd5e51ba694f65fa3fcfb0db571979
jjsemaan <>
2023-07-22 10:58:23 +0000

    Add validation to check all values are integers

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -17,6 +17,7 @@ sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
 
 print(data)
+
 
 def get_sales_data():
     """
@@ -31,22 +32,20 @@ def get_sales_data():
     sales_data = data_str.split(",")
     validate_data(sales_data)
 
+
 def validate_data(values):
     """
     Check if exactly six values are enterd.
     Check if these can be converted to ingers
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required and you provided only {len(values)}"
-    
             )
     except ValueError as e:
         print(f"Invalid data {e}, please try again.\n")
 
+
 get_sales_data()
-
-
-
-
